7ea9e7ff455724b9fb076399040dd3d3a7743ab1
wenchonglee <18256786+wenchonglee@users.noreply.github.com>
2023-11-03 16:32:24 +0800

    add github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { AppShell, Burger, NavLink } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { IconBrandGithubFilled } from "@tabler/icons-react";
 import { useState } from "react";
 import { DX } from "./DX";
 import { Conditional } from "./conditional/Conditional";
@@ -18,7 +19,6 @@ const data = [
 function App() {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [opened, { toggle }] = useDisclosure();
-
   return (
     <AppShell
       header={{ height: opened ? 40 : 1 }}
@@ -33,6 +33,14 @@ function App() {
         <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
       </AppShell.Header>
       <AppShell.Navbar>
+        <NavLink
+          label="Github"
+          href="https://github.com/wenchonglee/css"
+          description="Full source code"
+          p="md"
+          leftSection={<IconBrandGithubFilled size={32} />}
+          target="_blank"
+        />
         {data.map((item, index) => (
           <NavLink
             key={item.label}
